f42115bb1e014d75e122b221537863c217358b2a
AVCoding <70842251+AVCoding@users.noreply.github.com>
2022-05-18 11:19:02 +0300

    refresh

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
     window.onload = async function() { 
 
+    	// copy info from functions.js
+    	var info = {
+		    'env': asmLibraryArg,
+		    'wasi_snapshot_preview1': asmLibraryArg,
+		  };
 
+		let wasmMemory =  new WebAssembly.Memory({ initial: 256, maximum: 256}); // Wasm memory 
+
+		let wasmExports = null;
      	let response = await fetch('functions.wasm');
-        let bytes = await response.arrayBuffer();
-        let wasmObj = await WebAssembly.instantiate(bytes, info);
-        wasmExports = wasmObj.instance.exports;
+        let bytes = await response.arrayBuffer(); // convert file into a byte array
+        let wasmObj = await WebAssembly.instantiate(bytes, info); // create a new WebAssembly object
+        wasmExports = wasmObj.instance.exports; // exported functions
         console.log(wasmExports);
 
         document.getElementById('getSqrbtn').addEventListener("click", function(){
