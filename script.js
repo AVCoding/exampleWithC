7f2c49b517bccbb0619c5468fab0bc24d9e8ff63
AVCoding <70842251+AVCoding@users.noreply.github.com>
2022-05-19 17:49:48 +0300

    update

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
     window.onload = async function() { 
+	    
+	// copy asmLibraryArg from functions.js
+	var asmLibraryArg = {};
 
     	// copy info from functions.js
     	var info = {
-		    'env': asmLibraryArg,
-		    'wasi_snapshot_preview1': asmLibraryArg,
-		  };
+	    'env': asmLibraryArg,
+	    'wasi_snapshot_preview1': asmLibraryArg,
+	  };
 
-		let wasmMemory =  new WebAssembly.Memory({ initial: 256, maximum: 256}); // Wasm memory 
+	let wasmMemory =  new WebAssembly.Memory({ initial: 256, maximum: 256}); // Wasm memory 
 
-		let wasmExports = null;
+	let wasmExports = null;
      	let response = await fetch('functions.wasm');
         let bytes = await response.arrayBuffer(); //convert file into a byte array
         let wasmObj = await WebAssembly.instantiate(bytes, info); //create a new WebAssembly object
@@ -19,7 +22,7 @@
         	document.getElementById('result').innerText =   wasmExports.getSqr( document.getElementById('sqrinput').value ); 
         });
 
-	//2-nd version
+	//2-nd version , with functions.js
         //document.getElementById('getSqrbtn').addEventListener("click", function(){
         	//document.getElementById('result').innerText =  _getSqr( document.getElementById('sqrinput').value ); 
         //});
